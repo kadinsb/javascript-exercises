49024f005371e5ca5f9307cce3d2cb5978840b14
kadinsb <kadinbillington@gmail.com>
2025-07-11 18:39:12 +0800

    Completed js excersize 07 - sumAll

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,4 +1,37 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+
+
+    let sum = 0;
+    let firstNum = num1;
+    let secondNum = num2;
+
+
+if(Number.isInteger(firstNum) === false || Number.isInteger(secondNum) === false ){
+    return `ERROR`;
+}
+if(firstNum < 0 || secondNum < 0){
+    return `ERROR`;
+}
+
+    
+    if ( firstNum === secondNum) {
+        return sum = firstNum + secondNum;
+    }
+    else if ( firstNum < secondNum){
+        while (firstNum <= secondNum){
+            sum += firstNum;
+            firstNum++;
+        }
+    }
+    else if ( secondNum < firstNum){
+        while (secondNum <= firstNum){
+        sum += secondNum;
+        secondNum++;
+        }
+    }
+    
+
+    return sum;
 
 };
 
