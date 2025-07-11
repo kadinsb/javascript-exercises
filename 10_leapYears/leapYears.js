f846f377b40ca2aa378b29480a418d349f940b1f
kadinsb <kadinbillington@gmail.com>
2025-07-12 00:45:48 +0800

    Completed js excercise 10 & 11

--- 10_leapYears/leapYears.js
+++ 10_leapYears/leapYears.js
@@ -1,4 +1,27 @@
-const leapYears = function() {
+const leapYears = function(year) {
+
+    ///convert year to string and treat like an array
+    const yearString = (year.toString());
+
+    
+    ///determine IF year is in the 100's
+    if (yearString.length >= 3 && yearString.at(-1) === `0` && yearString.at(-2) === `0`){
+       
+        ///if year is divisible by 400 and returns int, return true
+        if (year % 400 === 0){
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+    ///check if year is divisible by 4 and returns an int
+    if(year % 4 === 0){
+        return true;
+    }
+    else {
+        return false;
+    }
 
 };
 
--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,17 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(f) {
+
+    const celsius = (f - 32) * (5/9);
+    const roundedCelcius = Math.round(celsius * 10)/10;
+    return roundedCelcius;
+
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(c) {
+
+  const farenheit = (c * (9/5)) + 32
+  const roundedFarenheit = Math.round(farenheit * 10)/10;
+  return roundedFarenheit;
+
 };
 
 // Do not edit below this line
